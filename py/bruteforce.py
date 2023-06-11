5b2bcf9738bfcbdaa9cdb911534021b50027f18d
Saleh Trissi <rayatrissi1@gmail.com>
2023-06-11 13:55:04 +0200

    Created def maximize_profit by of bruteforce

--- py/bruteforce.py
+++ py/bruteforce.py
@@ -1,4 +1,6 @@
 import csv
+from itertools import combinations
+import time
 
 
 def read_csv_file(file_path):
@@ -30,4 +32,55 @@ def read_csv_file(file_path):
             shares_list.append((action, cost, profit))
 
     return shares_list
- 
+
+
+def calculate_total_cost(combination):
+    """
+    Calculates the total cost of a combination of shares.
+
+    Args:
+        combination (tuple): A combination of shares as a tuple (action, cost, profit).
+
+    Returns:
+        float: Total cost of the combination.
+    """
+    return sum(action[1] for action in combination)
+
+
+def calculate_total_profit(combination):
+    """
+    Calculates the total profit of a combination of shares.
+
+    Args:
+        combination (tuple): A combination of shares as a tuple (action, cost, profit).
+
+    Returns:
+        float: Total profit of the combination.
+    """
+    return sum(action[1] * action[2] / 100 for action in combination)
+
+
+def maximize_profit(shares_list, max_budget):
+    """
+    Maximizes the profit for a given list of shares and maximum budget.
+
+    Args:
+        shares_list (list): List of shares as tuples (action, cost, profit).
+        max_budget (float): Maximum budget for the investment.
+
+    Returns:
+        tuple: Best investment options as a tuple (combination, profit).
+    """
+    max_profit = 0
+    best_investment = ()
+
+    # Generate combinations of shares
+    for i in range(len(shares_list)):
+        for combination in combinations(shares_list, i):
+            total_cost = calculate_total_cost(combination)
+            if total_cost <= max_budget:
+                total_profit = calculate_total_profit(combination)
+                if total_profit > max_profit:
+                    max_profit = total_profit
+                    best_investment = combination
+    return best_investment, max_profit
